87599cb483b6ee9d6b0120c04f54b234dc9accb7
Anders Fritz Fischer <Anders.Fischer1@googlemail.com>
2026-02-22 12:55:04 +0100

    Add music

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@
 // Use this renderer to draw into this window every frame.
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
+static SDL_AudioStream* audio = NULL;
+static Uint8* wav_data = NULL;
+static Uint32 wav_data_len = 0;
 
 constexpr auto WINDOW_WIDTH = 640;
 constexpr auto WINDOW_HEIGHT = 480;
@@ -20,9 +23,12 @@ std::unique_ptr<Game> game = nullptr;
 // This function runs once at startup.
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
-    SDL_SetAppMetadata("Spooky Memory", "0.1", "com.example.renderer-textures");
+    SDL_SetAppMetadata("Spooky Memory", "0.1", "com.example.spookymemory");
 
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_AudioSpec spec;
+    char* wav_path = NULL;
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -33,6 +39,21 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
     }
     //SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_INTEGER_SCALE);
     
+    SDL_asprintf(&wav_path, "%sassets/spooky.wav", SDL_GetBasePath());
+    if (!SDL_LoadWAV(wav_path, &spec, &wav_data, &wav_data_len)) {
+        SDL_Log("Couldn't load .wav file: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_free(wav_path);
+
+    audio = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, NULL, NULL);
+    if (!audio) {
+        SDL_Log("Couldn't create audio stream: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_ResumeAudioStreamDevice(audio);
+
     game = std::make_unique<Game>(window, renderer, WINDOW_WIDTH, WINDOW_HEIGHT);
     game->Init();
     
@@ -59,13 +80,15 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
     game->Update();
-
+    if (SDL_GetAudioStreamQueued(audio) < (int)wav_data_len) {
+        SDL_PutAudioStreamData(audio, wav_data, wav_data_len);
+    }
     return SDL_APP_CONTINUE;  // carry on with the program!
 }
 
 // This function runs once at shutdown.
 void SDL_AppQuit(void* appstate, SDL_AppResult result)
 {
-    
+    SDL_free(wav_data);
     // SDL will clean up the window/renderer for us.
 }
