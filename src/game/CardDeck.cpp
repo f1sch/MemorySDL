724642d3de2b1495fc97b096f91d230772e0eb36
Anders Fritz Fischer <Anders.Fischer1@googlemail.com>
2026-03-11 20:35:03 +0100

    Bug: Matched cards flip back

--- src/game/CardDeck.cpp
+++ src/game/CardDeck.cpp
@@ -7,7 +7,6 @@ void CardDeck::BuildDeck(const std::vector<std::string> &frontKeys)
 {
     m_cards.clear();
     m_cards.reserve(frontKeys.size() * 2);
-    //m_cards.resize(frontKeys.size() * 2);
 
     int pairId = 0;
     int uniqueId = 0;
@@ -16,9 +15,6 @@ void CardDeck::BuildDeck(const std::vector<std::string> &frontKeys)
         CardData data {uniqueId++, pairId, key, 0.0f};
         m_cards.push_back(data);
         m_cards.push_back(data);
-        // Card a; a.uniqueId = uniqueId++; a.pairId = pairId; a.frontKey = key;
-        // Card b; b.uniqueId = uniqueId++; b.pairId = pairId; b.frontKey = key;
-        // m_cards.push_back(b);
         ++pairId;
     }
 }
